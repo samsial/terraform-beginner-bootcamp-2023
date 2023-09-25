83ae5e3190c0439b924f3e9ed6c9d08433c5a444
Andrew Paul <andrew.paul2@hotmail.com>
2023-09-25 19:14:31 +0000

    #23 imported lost resources updated to remove random.string in bucket name hardcoded the bucket name for website_bucket

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 output "random_bucket_name" {
-    value = random_string.bucket_name.result
+    value = aws_s3_bucket.website_bucket
 }
